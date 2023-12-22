49fea52447c704a69f65c6161cb4268df4a071fb
tetsu-m7 <14990439+tetsu-m7@users.noreply.github.com>
2023-12-22 12:30:10 -0800

    almost finished creating gets3file.py

--- gets3file.py
+++ gets3file.py
@@ -1,32 +1,93 @@
 import os
 import fcntl
 import sys
-import boto3
-import configparser
 import socket
 import gzip
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 from datetime import datetime, timedelta, timezone
+import pandas as pd
 
-# AWS S3クライアントのセットアップ
-s3 = boto3.client('s3')
-
-# ロックファイルを設定
 lock_file = "script_lock.lock"
 
-# メール設定
-smtp_server = 'smtp.gmail.com'
-smtp_port = 587
-smtp_user = 'your-email@gmail.com'  # 送信元メールアドレス
-smtp_password = 'your-email-password'  # 送信元メールアドレスのパスワード
-smtp_receiver = 'receiver-email@example.com'  # 受信先メールアドレス
+# S3 bucket setting
+status_file_path = 'status_file.csv'
+logs_dir_suffix = 'logs/'
+syslog_server = 'localhost'
+syslog_port = 514
 
-# S3のバケット設定
-source_bucket = 'your-source-bucket'
-source_directory = 'your-source-directory'
-config_bucket = 'your-config-bucket'
+
+# Main processing
+def main():
+    # 1. Exclusive control
+    lock_fd = acquire_lock()
+    if lock_fd is None:
+        print("The script is already running. Exiting.")
+        sns_topics("gets3file.py: The script is already running. Exiting.", "test")
+        sys.exit(1)
+
+    try:
+        # 2. Define start/end time based on proceeded file.
+        #   start_time : latest time - 1hour
+        #   end_time   : current time
+        #   time_list  : time list with 5 interval between start_time and end_time
+        proceeded_status = []
+        if not os.path.exists(status_file_path) or not os.path.getsize(status_file_path) > 0:
+            start_time = datetime.now()
+        else:
+            with open(status_file_path, "r") as file:
+                for line in file:
+                    proceeded_status.append(line.strip())
+            proceeded_time_list = []
+            for proceeded_file in proceeded_status:
+                date_str = proceeded_file.split("/")[1:6]
+                date_object = datetime.strptime('/'.join(date_str), "%Y/%m/%d/%H/%M")
+                proceeded_time_list.append(date_object)
+            start_time = max(proceeded_time_list) - timedelta(hours=1)
+        print(start_time)
+        # end_time = datetime.now() # debug
+        end_time = datetime(2023, 9, 1, 19, 30)  # debug
+        current_time = start_time
+        time_list = []
+        while current_time <= end_time:
+            time_list.append(current_time.strftime("%Y/%m/%d/%H/%M"))
+            current_time += timedelta(minutes=5)
+        print(time_list)
+        # 3. Define file path based on time_list
+        # file_path : absolute file path under specific time directory
+        file_path = []
+        for time_target in time_list:
+            directory_path = logs_dir_suffix + time_target
+            if os.path.exists(directory_path) and os.path.isdir(directory_path):
+                file_list = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+                file_path_list = [os.path.join(directory_path, file) for file in file_list]
+                for file_path_target in file_path_list:
+                    file_path.append(file_path_target)
+        print(file_path)
+
+        # 4. Define target file path
+        # target_files : (absolute file path under specific time directory) - (proceeded status file)
+        target_files = [element for element in file_path if element not in proceeded_status]
+        print(target_files)
+
+        # 5. Read file and send syslog
+        try:
+            for gz_file in target_files:
+                with gzip.open(gz_file, 'rt') as f:
+                    logs = f.read()
+                    print(logs)
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                    sock.sendto(logs.encode('utf-8'), ("localhost", 514))
+                print("Send Syslog")
+        except Exception:
+            sns_topics("gets3file.py: FileOpen or Syslog Error", "test")
+
+        # 6. Add data to proceeded file.
+        with open(status_file_path, 'a') as file:
+            for line in target_files:
+                file.write(line + '\n')
+
+    finally:
+        release_lock(lock_fd)
+
 
 # Create a lock file and check that no other instance is running
 def acquire_lock():
@@ -37,124 +98,28 @@ def acquire_lock():
     except (IOError, BlockingIOError):
         return None
 
+
 # Release the lock file
 def release_lock(lock_fd):
     if lock_fd:
         fcntl.flock(lock_fd, fcntl.LOCK_UN)
         lock_fd.close()
 
-# Send an email in case of failure
-def send_email(subject, body):
-    msg = MIMEMultipart()
-    msg['From'] = smtp_user
-    msg['To'] = smtp_receiver
-    msg['Subject'] = subject
 
-    msg.attach(MIMEText(body, 'plain'))
-
+def sns_topics(subject, body):
+    sns_client = boto3.client('sns', region_name='YOUR_REGION')
+    topic_arn = 'arn:aws:sns:YOUR_REGION:YOUR_ACCOUNT_ID:YOUR_TOPIC_NAME'
+    custom_message = body
     try:
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(smtp_user, smtp_password)
-        server.sendmail(smtp_user, smtp_receiver, msg.as_string())
-        server.quit()
+        response = sns_client.publish(
+            TopicArn=topic_arn,
+            Message=custom_message,
+            Subject=subject  # オプションの件名
+        )
+        print(f"Message sent successfully. Message ID: {response['MessageId']}")
     except Exception as e:
-        print(f"Failed to send email: {str(e)}")
+        print(f"Error sending message: {e}")
 
-# S3からファイルの内容を取得し、gzip圧縮を解凍して返す
-def get_file_content(bucket_name, file_name):
-    try:
-        obj = s3.get_object(Bucket=bucket_name, Key=file_name)
-        content = obj['Body'].read()
-        if file_name.endswith('.gz'):
-            content = gzip.decompress(content).decode('utf-8')
-        return content
-    except Exception as e:
-        print(f"Failed to get file content: {str(e)}")
-        return None
-
-# Download the config file from S3
-def download_config_file():
-    try:
-        s3.download_file(config_bucket, 'config.ini', '/tmp/config.ini')
-        return True
-    except Exception as e:
-        print(f"Failed to download config file: {str(e)}")
-        return False
-
-# Upload the config file to S3
-def upload_config_file():
-    try:
-        s3.upload_file('/tmp/config.ini', config_bucket, 'config.ini')
-        return True
-    except Exception as e:
-        print(f"Failed to upload config file: {str(e)}")
-        return False
-
-# Main processing
-def main():
-    lock_fd = acquire_lock()
-    if lock_fd is None:
-        print("The script is already running. Exiting.")
-        sys.exit(1)
-
-    try:
-        # Download the config file from S3
-        if download_config_file():
-            config_file_path = '/tmp/config.ini'
-        else:
-            print("Failed to download the config file. Exiting.")
-            sys.exit(1)
-
-        # Set the S3 bucket and directory
-        source_bucket = 'your-source-bucket'
-        source_directory = 'your-source-directory'
-        
-        # Get a list of files and timestamps from S3
-        files_and_timestamps = list_files_in_s3_directory(source_bucket, source_directory)
-
-        # Extract files with timestamps within the last 30 minutes
-        threshold = datetime.now(timezone.utc) - timedelta(minutes=30)
-        recent_files = [(file, timestamp) for file, timestamp in files_and_timestamps if timestamp >= threshold]
-
-        # Get the list of files to process from the Config file
-        config_files = get_config_files(config_file_path)
-
-        # Identify missing files
-        missing_files = [file for file, _ in recent_files if file not in config_files]
-
-        # Send Syslog messages
-        syslog_server = 'localhost'
-        syslog_port = 514
-
-        for file in missing_files:
-            syslog_message = f"File {file} is missing in the config."
-            send_syslog_message(syslog_server, syslog_port, syslog_message)
-
-            # Add the file name to the Config file
-            add_file_to_config(config_file_path, file)
-
-            # Get the file content and send it via Syslog
-            file_content = get_file_content(source_bucket, file)
-            if file_content:
-                send_syslog_message(syslog_server, syslog_port, file_content)
-
-        # Upload the updated Config file to S3
-        if upload_config_file():
-            print("Config file uploaded to S3.")
-        else:
-            print("Failed to upload the config file to S3.")
-
-    except Exception as e:
-        # Send an email with the error message in case of failure
-        subject = "Script Error"
-        body = f"An error occurred in the script:\n\n{str(e)}"
-        send_email(subject, body)
-
-    finally:
-        # Release the lock file
-        release_lock(lock_fd)
 
 if __name__ == '__main__':
     main()
-
